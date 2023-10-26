4f3ba621136b8b6508d5b2d966a3075c59cd0530
Leonardo Acevedo <stenoner90@gmail.com>
2023-10-25 23:50:45 -0500

    implemented scroll bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Scroll from '@/components/ui/Scroll'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,12 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <body className={inter.className}>
-          <Header />
-          <main className='flex min-h-screen w-full flex-col p-10'>
-            {children}
-          </main>
-        </body>
+      <Scroll />
+      <body className={inter.className}>
+        <Header />
+        <main className='flex min-h-screen w-full flex-col p-10'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
